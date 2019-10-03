c6948d159ed9cd6bc6aabcd918bc35a00e7b75a9
Megatron997 <Megatron997@mail.ru>
2019-10-03 18:38:40 +0300

    1
    1

--- php/beautyTitle.php
+++ php/beautyTitle.php
@@ -5,7 +5,7 @@ $title= (int)$_GET['title']+1;
 $partsRome = array("I", "II", "III", "IV", "V");
 $title=$DB->query("SELECT title FROM MENU WHERE id =".$title.";")->fetchArray()[0];
 echo "<title class='main-text-only-one' id=".(int)$_GET['title'].$_GET['part'].">".$title.": Part — ".$partsRome[$part-1]."</title>";
-echo "<h1 align='center' class='beauty_title''>".$title.": Part - ".$partsRome[$part-1]."</h1>";
+echo "<h2 align='center' class='beauty_title''>".$title.": Part - ".$partsRome[$part-1]."</h2>";
 echo "<div class=\"divider\"></div>";
 ?>
 
---
+++ student-book.php
@@ -0,0 +1,134 @@
+<html>
+
+<head>
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection"/>
+
+    <link rel="stylesheet" href="css/styles3.css" >
+    <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <script>
+        $( document ).ready(function() {
+            $(".dropdown-trigger").dropdown();
+        });
+    </script>
+</head>
+
+<body>
+
+
+<ul id="dropdown1" class="dropdown-content">
+    <li><a href="#!">one</a></li>
+    <li><a href="#!">two</a></li>
+    <li class="divider"></li>
+    <li><a href="#!">three</a></li>
+</ul>
+<nav>
+
+    <div class="nav-wrapper">
+
+        <a href="https://sfedu.ru/" class="brand-logo"><img src="logo.png" width="65vw" height="65vw"></a>
+
+
+        <ul id="nav-mobile" class="right hide-on-med-and-down valign-wrapper">
+
+            <li><a href="index.html">Home</a></li>
+
+            <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">About<i class="material-icons right">arrow_drop_down</i></a></li>
+
+        </ul>
+    </div>
+
+</nav>
+
+
+
+<div class="content">
+
+    <div class="container">
+
+        <h4 style="padding-top: 2vh" align="center">STUDENT’S BOOK FOR CLASSROOM USE </h4>
+
+        <h4  style="color: #dc3545;" align="center">“Learn the English of Physics” </h4>
+
+        <h5 class="blueAboutText">Целевой пользователь – Target user</h5>
+
+        <p>Студенты  1-4 курсов бакалавриата и частично  1-2 го года магистратуры физических и других естественнонаучных специальностей университетов со средним уровнем (B1-B2) владения английским языком.</p>
+
+        <h5 class="blueAboutText">Цели – objectives</h5>
+
+        <p>Формирование иноязычных коммуникативных компетенций для:</p>
+        <ul class="myul" style="list-style-type: square;">
+            <li class="lisher">использования языковых знаний и умений в различных ситуациях академического и профессионального общения;  </li>
+            <li class="lisher">развития творческого подхода к решению учебных и профессиональных задач;
+                формирования умений самостоятельной работы;  </li>
+            <li class="lisher">активного использования современных информационных технологий; </li>
+            <li class="lisher">коллективной познавательной деятельности;  </li>
+            <li class="lisher">самоконтроля и оценки усвоения формируемых навыков и умений.</li>
+        </ul>
+        <div class="row">
+            <div class="col s2"></div>
+            <div class="col s4"></div>
+            <div class="col s4"></div>
+            <div class="col s2"></div>
+        </div>
+        <div class="row">
+            <div class="col s9">
+        <p>Кнопка/иконка PDF на каждой странице позволяет открыть и/или скачать <b>соответствующие разделы</b> из учебника “Learn the English of Physics”.  </p>
+        <p>Для получения <b>PDF копии всего учебника</b> отправляйте запрос на электронную почту <a href="mailto:elena.i.grushko@gmail.com?subject=Получение PDF копии">elena.i.grushko@gmail.com</a> </p>
+            </div>
+            <div class="col s1"></div>
+            <div class="col s2" style="height:10vh">
+                <div class="valign-wrapper">
+                <img src="pdf.png" alt="Открыть">
+                    </div>
+            </div>
+            </div>
+        </div>
+
+</div>
+
+<footer class="page-footer">
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col l6 s12">
+
+                <h5 class="white-text"></h5>
+
+                <p class="grey-text text-lighten-4">????.</p>
+
+            </div>
+
+            <div class="col l4 offset-l2 s12">
+
+                <h5 class="white-text">Links</h5>
+
+                <ul>
+
+                    <li><a class="grey-text text-lighten-3" href="https://vk.com/null.pointer.exception">Sergei Chamkin</a></li>
+
+                    <li><a class="grey-text text-lighten-3" href="https://vk.com/vanyamakarov">Ivan Makarov</a></li>
+
+                </ul>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="footer-copyright">
+
+        <div class="container">2019 All rights reserved</div>
+
+    </div>
+
+</footer>
+
+</body>
+
+</html>
